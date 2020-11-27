e55b4f87c9cbae54bd77a7a7054995df53fbaf7d
David <davidbrown3142@gmail.com>
2020-11-27 10:04:40 -0500

    add-user working

---
+++ commands/add-user.js
@@ -0,0 +1,11 @@
+const db = require("../models");
+module.exports = {
+	name: 'add-user',
+	description: 'Add user to db.',
+	execute(message, args) {
+		db.User.create({
+            uid: message.author.id,
+            money: 500
+        });
+	},
+};

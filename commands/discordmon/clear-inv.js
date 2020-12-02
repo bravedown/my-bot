c85d520e7b42ddb66f423a75519bdaf2c49c7356
David <davidbrown3142@gmail.com>
2020-12-02 17:54:55 -0500

    update instead of destroy

--- commands/discordmon/clear-inv.js
+++ commands/discordmon/clear-inv.js
@@ -3,7 +3,8 @@ module.exports = {
 	name: 'clear-inv',
 	description: 'Release all your dmon!',
 	execute(message, args) {
-		db.DMonInv.destroy({
+		db.DMonInv.update({quantity: 0},
+            {
             where: {
                 UserId: message.author.id
             }

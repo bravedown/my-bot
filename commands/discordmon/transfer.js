f20f66aaa1c203b5317ece3c5eb31b2c172873b8
David <davidbrown3142@gmail.com>
2020-12-02 18:20:48 -0500

    imports, other small refactors

--- commands/discordmon/transfer.js
+++ commands/discordmon/transfer.js
@@ -1,26 +1,27 @@
 const db = require("../../models");
+const catchDMon = require('../../functions/catch-dmon');
 module.exports = {
 	name: 'transfer',
     description: 'Check your dmon inventory. Mention someone to get their inventory instead!',
     args: true,
 	execute(message, args) {
         let [rarity, name, quantityToTransfer] = args.slice(1);
-        db.Discordmon.findAll({
+        db.Discordmon.findOne({
             where: {
                 rarity: rarity,
                 name: name
             }
         }).then(res => {
             if (res.length === 0) return;
-            let dMonId = res[0].id;
-            db.DMonInv.findAll({
+            let dMonId = res.id;
+            db.DMonInv.findOne({
                 where: {
                     UserId: message.author.id,
                     DiscordmonId: dMonId
                 }
             }).then(resp => {
                 if (resp.length === 0) return;
-                let ownedQuantity = resp[0].quantity;
+                let ownedQuantity = resp.quantity;
                 if (ownedQuantity >= quantityToTransfer) {
                     db.DMonInv.update({quantity: ownedQuantity - quantityToTransfer}, {
                         where: {
@@ -28,9 +29,9 @@ module.exports = {
                             DiscordmonId: dMonId
                     }}).then(response => {
                         let mentionId = message.mentions.users.first().id;
-                        require('../../functions/catch-dmon')(mentionId, dMonId, +quantityToTransfer);
+                        catchDMon(mentionId, dMonId, +quantityToTransfer);
                         message.reply('transferred');
-                    })
+                    });
                 }
             })
         });

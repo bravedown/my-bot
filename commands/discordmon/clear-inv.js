3a1313ecf533d306957f5cba25e477afca092169
David <davidbrown3142@gmail.com>
2020-12-02 18:14:32 -0500

    spacing refactor

--- commands/discordmon/clear-inv.js
+++ commands/discordmon/clear-inv.js
@@ -3,13 +3,9 @@ module.exports = {
 	name: 'clear-inv',
 	description: 'Release all your dmon!',
 	execute(message, args) {
-		db.DMonInv.update({quantity: 0},
-            {
-            where: {
-                UserId: message.author.id
-            }
-        }).then(res => {
-            message.reply(`inventory cleared!`);
-        });
+        db.DMonInv.update({quantity: 0}, {where: {UserId: message.author.id}})
+            .then(res => {
+                message.reply(`inventory cleared!`);
+            });
 	},
 };
